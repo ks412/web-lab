f31f3e95fc75652cac167b02d69002d9c3e7ac41
Evgeniy <domcnc412@gmail.com>
2022-03-23 20:24:52 +0200

    NewStyleJS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+function ElemensDispleyNone(idElemets){
+
+    idElemets.style.display = 'none';
+
+}
+
+const inputMail = document.getElementById('mail');
+const InpitButtRegister = document.getElementById('register');
+//const linkList = document.getElementById('link__list');
+
+
+
+ElemensDispleyNone(inputMail)
+ElemensDispleyNone(InpitButtRegister)
+
